3192375af0704f4f17c9033f012ab234db6c36d2
zakozak <zakozak1997@gmail.com>
2019-09-20 18:58:43 +0200

    commit 3

--- src/main/java/com/project/otf/GreetingController.java
+++ src/main/java/com/project/otf/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -31,10 +32,23 @@ public class GreetingController {
         return "main";
     }
     @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, String name, Map<String, Object> model){
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
         Message message = new Message(text, tag);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter (@RequestParam String filter, Map<String, Object> model){
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()){
+             messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
         model.put("messages", messages);
         return "main";
     }
--- src/main/java/com/project/otf/repos/MessageRepo.java
+++ src/main/java/com/project/otf/repos/MessageRepo.java
@@ -3,5 +3,9 @@ package com.project.otf.repos;
 import com.project.otf.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message,Long> {
+
+    List<Message> findByTag(String tag);
 }
